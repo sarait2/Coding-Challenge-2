595251ed833dd5eedd0bea73d427ceff46274d95
sarait2 <saraitorres999@gmail.com>
2024-09-08 18:37:22 -0400

    bill_splitter.js

--- bill_splitter.js
+++ bill_splitter.js
@@ -1,2 +1,24 @@
-// Task 1 - Calculate the Tip
-console.log()
+/ Task 1 - Calculate the Tip - Ternary Operator
+// Example bill cost
+let bill = x
+let tip= (bill>=50, bill <=300) ? bill * 0.15 : bill * 0.2;
+let total= bill + tip;
+
+// Task 2 - Output Details
+bill = 275
+let tip= (bill>=50, bill <=300) ? bill * 0.15 : bill * 0.2;
+let total = bill + tip;
+console.log ("The bill was $275, the tip was $41.25, and the total value is $316.25") 
+
+//Task 3 - Function
+function calculatetip ( if (bill <=50)* .15 : if (bill >=300)*.2 ) {}
+
+// Task 4 - Arrays (data set 1)
+Array bills [275, 40, 420]
+function calculatetip 
+Array total
+
+// Data set 2
+Array bills [125, 555, 44]
+function calculatetip
+array total
